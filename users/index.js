892b994a91e8b1bfcd316e7549419dd322d705f5
Bala <sbalasubramanian96@gmail.com>
2020-05-11 12:47:56 +0530

    feat: Added morgon file logger

--- users/index.js
+++ users/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const logger = require("./middleware/logger");
+const fs = require("fs");
 const authenticateJWT = require("./middleware/auth");
 const { catch404, errorHandler } = require("./middleware/errorHandler");
 const cors = require("cors");
@@ -8,10 +8,17 @@ const cookieParser = require("cookie-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./docs/swagger.json");
 const swaggerDocument2 = require("./docs/swaggerV2.json");
+const logger = require("morgan");
+
 const app = express();
 
-// Init middleware
-app.use(logger);
+// Init middlewares
+app.use(
+  logger("common", {
+    stream: fs.createWriteStream("./access.log", { flags: "a" }),
+  })
+);
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api-docs/v1", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api-docs/v2", swaggerUi.serve, swaggerUi.setup(swaggerDocument2));
+
 app.use(authenticateJWT);
 
 // DB Connection
